eebf7763b3d842c910e9a0801b00dfa0fbb782a3
Angela <angelahdezd@gmail.com>
2013-11-26 14:12:15 +0000

    mostrar mi posicion

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
   $(".cell").click(function(e) {
-  	alert("click");
-    $.get(e.srcElement.id, function(data) {
-      console.log(data);
-      alert(data);
-     
+   alert(e.target.id);
+    $.get(e.target.id, function(data) {
+     alert(data);
       //alert( "Load was performed." );
     });
   });
