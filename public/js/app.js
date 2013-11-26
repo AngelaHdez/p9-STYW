8e5642cf9f4622f88e5138348be6fc5807c9a1ff
Angela <angelahdezd@gmail.com>
2013-11-26 14:15:12 +0000

    modificando ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".cell").click(function(e) {
-   alert(e.target.id);
-    $.get(e.target.id, function(data) {
-     alert(data);
+  	//alert(e.target.id);
+    $.get(e.srcElement.id, function(data) {
+
       //alert( "Load was performed." );
     });
   });
