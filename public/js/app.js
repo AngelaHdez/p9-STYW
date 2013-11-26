fae47f5d0ef5c677ce8b201e4387106626baf2c2
Angela <angelahdezd@gmail.com>
2013-11-26 21:10:38 +0000

    se pintan pero no funciona correcto

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,22 @@ $(document).ready(function(){
   $(".cell").click(function(event) {
     alert(event.target.id);
     var pathname = window.location.pathname;
-    alert(pathname);
+    //alert(pathname);
    // alert(event.srcElement.id);
     var ruta =pathname + event.target.id;
     //alert("La ruta " + ruta);
 
     $.get(ruta, function(data) {
+      if (data != 'illegal') {
         alert("Datos "+data);
+        alert("Celda "+event.target.id);
+
+        $("#"+event.target.id).addClass("circle");
+        $("#"+data).addClass( "cross" );
+      }
+      else {
+        alert("Illegal move!")
+      }
     });
   });
 });
